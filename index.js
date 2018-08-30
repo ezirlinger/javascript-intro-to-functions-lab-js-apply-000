86200536d5bc7e63008d2f0e7f6bc3b5d73ea836
eric.zirlinger@gmail.com <eric.zirlinger@gmail.com>
2018-08-30 15:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
-// Output is string should be uppercase by using the toUpperCase() method
+
+
+/* describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+*/
+
+
+/* TEST 1
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})*/
+
+//TEST 1 Output =  string should be uppercase by using the toUpperCase() method
 function shout(string) {
   return string.toUpperCase()
 }
 
-// Output is string should be lowercase using the toLowerCase() method
+/* TEST 2
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})*/
+
+// TEST 2 Output = string should be lowercase using the toLowerCase() method
 function whisper(string) {
   return string.toLowerCase()
 }
 
+/* TEST 3
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})*/
+
+// TEST 3 Output =
+
+function logShout(string) {
+  console.log(shout);
+}
