1ba16acbef2c73d9ddea78cd5ca43edd7a56a48a
eric.zirlinger@gmail.com <eric.zirlinger@gmail.com>
2018-08-30 16:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,11 +76,11 @@ describe('sayHiToGrandma(string)', function() {
 
 // TEST 5 Output TBD
 function sayHiToGrandma(string) {
-  if string.toLowerCase()==string {
+  if (string.toLowerCase()==string) {
     return "I can\'t hear you!";
-  } else if string.toUpperCase()==string {
+  } else if (string.toUpperCase()==string) {
     return "YES INDEED!";
-  } else if string== "I love you, Grandma." {
+  } else if (string== "I love you, Grandma.") {
     return "I love you, too."
     }
   }
